a3d5e81e1ff59f701beef588d65ea19811fcaa49
SimoCharhbili <simocharhbili@outlook.com>
2022-06-20 14:32:25 +0100

    build: initial commit

---
+++ components/navigation/header/Header.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+
+export interface IHeader extends React.ComponentPropsWithoutRef<'header'> {}
+
+const Header: React.FC<IHeader> = ({ className, ...headerProps }) => {
+  return (
+    <header
+      {...headerProps}
+      className={`w-full flex flex-row justify-between ${className}`}
+    >
+      <div className="space-x-5 m-5">
+        <Link href="/">
+          <a className="hover:underline">Home</a>
+        </Link>
+        <Link href="/">
+          <a className="hover:underline">Store</a>
+        </Link>
+      </div>
+      <div className="space-x-5 m-5">
+        <Link href="/">
+          <a className="hover:underline hidden sm:inline">Gmail</a>
+        </Link>
+        <Link href="/">
+          <a className="hover:underline hidden sm:inline">Images</a>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
